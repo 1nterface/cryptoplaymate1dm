4024fdedc42430525517f0f479817410d6787016
1nterface <1nterfacedemexico@gmail.com>
2022-02-03 19:47:09 -0800

    1.0.0k

---
+++ android/app/src/main/kotlin/com/example/cryptoplaymate1dm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cryptoplaymate1dm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+//@dart=2.9
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:location_permissions/location_permissions.dart';
+import 'package:cryptoplaymate1dm/admin_inicio.dart';
+import 'package:cryptoplaymate1dm/Clientes/clientes_login.dart';
+import 'package:cryptoplaymate1dm/Clientes/menu_cliente.dart';
+import 'package:cryptoplaymate1dm/Clientes/home.dart';
+import 'package:cryptoplaymate1dm/Clientes/lista_restaurantes.dart';
+import 'package:cryptoplaymate1dm/Clientes/registro.dart';
+import 'package:cryptoplaymate1dm/Modelo/cajas_modelo.dart';
+import 'package:cryptoplaymate1dm/Modelo/nota_modelo.dart';
+import 'package:cryptoplaymate1dm/olvidecontra.dart';
+
+import 'Modelo/panel_pedido_modelo.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setPathUrlStrategy();
+  // Initialize a new Firebase App instance
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  const MyApp({Key key}) : super(key: key);
+  
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.black,
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder> {
+        '/olvide_contra': (BuildContext context) => olvidecontra(),
+
+        '/clientes_login': (BuildContext context) => clientes_login(),
+        '/registro': (BuildContext context) => registro(),
+        '/home': (BuildContext context) => home(cajas_modelo("","","",0,0,0,0,0,"","","","","",0),0),
+        '/lista_restaurantes': (BuildContext context) => lista_restaurantes(),
+
+      },
+      title: 'Siento11 Colectivo',
+      theme: ThemeData(
+
+      ),
+      home:
+      home(cajas_modelo("","","",0,0, 0, 0, 0, "", "", "", "", "", 0),0),
+      //clientes_login(),
+      //menu_cliente("","","","","","",0,0),
+      // <--- App Clientes
+
+
+    );
+  }
+}
